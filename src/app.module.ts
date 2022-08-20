6e16ec243a4dceb63b5d1eee524c19b08e480fe4
sudac_tudak <30slava09@mail.ru>
2022-08-20 22:37:25 +0300

    install nestjs/config and remove app service and app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ReviewModule } from './review/review.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, ReviewModule, TopPageModule, ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    ReviewModule,
+    TopPageModule,
+    ProductModule,
+  ],
 })
 export class AppModule {}
