6c8af75b0df7f1d2edec49f104d3103144407893
sudac_tudak <30slava09@mail.ru>
2022-08-28 14:28:56 +0300

    Create a Dockerfile

--- src/review/review.service.spec.ts
+++ src/review/review.service.spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { exec } from 'child_process';
 import { Types } from 'mongoose';
 import { getModelToken } from 'nestjs-typegoose';
 import { ReviewService } from './review.service';
